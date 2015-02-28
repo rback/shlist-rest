c752ac4a2e8d9e12b2ac16e624b7e69fe6af5212
Rasmus Back <rasmus@meedoc.com>
2015-02-28 16:27:38 +0200

    Use App trait for less boilerplate

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -3,16 +3,14 @@ import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.ServletContextHandler
 import org.scalatra.servlet.ScalatraListener
 
-object JettyLauncher {
-  def main(args: Array[String]) {
-    val config = ConfigFactory.load(sys.props.get(org.scalatra.EnvironmentKey).getOrElse("local"))
+object JettyLauncher extends App {
+  val config = ConfigFactory.load(sys.props.get(org.scalatra.EnvironmentKey).getOrElse("local"))
 
-    val server = new Server(config.getInt("app.port"))
-    val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
-    context.setResourceBase("web/src/main/webapp")
-    context.addEventListener(new ScalatraListener)
-    server.setHandler(context)
-    server.start()
-    server.join()
-  }
+  val server = new Server(config.getInt("PORT"))
+  val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
+  context.setResourceBase("web/src/main/webapp")
+  context.addEventListener(new ScalatraListener)
+  server.setHandler(context)
+  server.start()
+  server.join()
 }
